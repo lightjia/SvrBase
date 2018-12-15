a1dc64c01da5db70418ec2864d2de028268a2382
lightjia <lightjia@163.com>
2018-12-15 11:11:28 +0800

    公共库修改

--- SvrBase/cfg/src/Config.cpp
+++ SvrBase/cfg/src/Config.cpp
@@ -1,59 +1,47 @@
 #include "Config.h"
 
-#define DEFAULT_CONF_DIR	"conf/"
-
-CConfig::CConfig()
-{
+CConfig::CConfig(){
 	m_bLoad = false;
 }
 
-CConfig::~CConfig()
-{
+CConfig::~CConfig(){
 }
 
-int CConfig::LoadJsonConf(const char* pData)
-{
-	if (nullptr == pData)
-	{
+int CConfig::LoadJsonConf(const char* pData){
+	if (nullptr == pData){
 		return 1;
 	}
 
 	rapidjson::Document doc;
 	doc.Parse<rapidjson::kParseDefaultFlags>(pData);
-	if (!doc.HasParseError())
-	{
+	if (!doc.HasParseError()) {
 		ParseJson(doc);
-	}
-	else
-	{
+	} else {
 		fprintf(stderr, "Parse Json File Error");
 	}
 
 	return 0;
 }
 
-int CConfig::LoadConfig(const char* pFileName, conf_file_type iType)
-{
-	std::string strConfFile = get_app_path() + DEFAULT_CONF_DIR + pFileName;
-	const long lFileLen = get_file_len(strConfFile.c_str(), "r");
-	if (lFileLen > 0)
-	{
+int CConfig::LoadConfig(const char* pFileName, conf_file_type iType){
+	const long lFileLen = get_file_len(pFileName, "r");
+	if (lFileLen > 0){
 		fprintf(stderr, "CConfig::LoadConfig malloc\n");
-		char* pFileData = (char*)do_malloc(lFileLen);
-		file_read(strConfFile.c_str(), "r", (unsigned char*)pFileData, lFileLen, 0);
-		if (iType == CONF_FILE_TYPE_JSON)
-		{
-			LoadJsonConf(pFileData);
-		}
-		else
-		{
-			fprintf(stderr, "error conf type:%d", iType);
+		char* pFileData = (char*)do_malloc(lFileLen + 1);
+		size_t iRead = file_read(pFileName, "rb", (unsigned char*)pFileData, lFileLen, 0);
+		if (iRead == lFileLen) {
+			pFileData[iRead] = '\0';
+			if (iType == CONF_FILE_TYPE_JSON){
+				LoadJsonConf(pFileData);
+			} else {
+				fprintf(stderr, "error conf type:%d", iType);
+			}
+		} else {
+			fprintf(stderr, "File:%s filelen:%ld readlen:%lld", pFileName, lFileLen, iRead);
 		}
 
 		DOFREE(pFileData);
-	}
-	else
-	{
+	} else {
 		fprintf(stderr, "file:%s is empty", pFileName);
 	}
 
