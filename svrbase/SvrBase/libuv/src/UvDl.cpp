804855935265835363d6ca7595edce6d30f3cfe1
huangjiajia <huangjiajia@hd.com>
2019-05-24 14:44:32 +0800

    Change The Dl

--- svrbase/SvrBase/libuv/src/UvDl.cpp
+++ svrbase/SvrBase/libuv/src/UvDl.cpp
@@ -14,13 +14,21 @@ const char* CUvDl::GetErr() {
 
 int CUvDl::Open(const char* pFileName) {
     ASSERT_RET_VALUE(pFileName && strlen(pFileName), 1);
-    return uv_dlopen(pFileName, &mstLib);
+	if (uv_dlopen(pFileName, &mstLib)) {
+		LOG_ERR("uv_dlopen error: %s", GetErr());
+		return 1;
+	}
+
+    return 0;
 }
 
 int CUvDl::GetFunc(const char* pFuncName, void** pFuncPtr) {
     ASSERT_RET_VALUE(pFuncPtr && pFuncName && strlen(pFuncName), 1);
+	if (uv_dlsym(&mstLib, pFuncName, pFuncPtr)) {
+		LOG_ERR("uv_dlsym error: %s", GetErr());
+	}
 
-    return uv_dlsym(&mstLib, pFuncName, pFuncPtr);
+    return 0;
 }
 
 void CUvDl::Close() {
